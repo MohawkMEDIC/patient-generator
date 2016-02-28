392746660f319793bf17e9f201285223838b834e
Nityan Khanna <nityan.khanna@mohawkcollege.ca>
2016-02-27 19:01:20 -0500

    starting to add models to the database

---
+++ PatientGenerator.Persistence/Model/NamePartType.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Copyright 2016-2016 Mohawk College of Applied Arts and Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you
+ * may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ * User: Nityan
+ * Date: 2016-2-27
+ */
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PatientGenerator.Persistence.Model
+{
+	internal class NamePartType
+	{
+		public NamePartType()
+		{
+		}
+
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+
+		[Required]
+		public NamePartTypeCode NamePartTypeCode { get; set; }
+
+		public int NamePartId { get; set; }
+
+		[ForeignKey("NamePartId")]
+		public virtual NamePart NamePart { get; set; }
+	}
+}
